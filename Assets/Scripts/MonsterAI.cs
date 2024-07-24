ed2d74eec67b20ef15dfd73c4995dda70654dff3
honnnney <tpgjs1023@gmail.com>
2024-07-24 15:32:58 +0900

    Monster AI
    반경 8 안에 플레이어가 들어오면 플레이어 쫓아가게

---
+++ Assets/Scripts/MonsterAI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterAI : MonoBehaviour
+{
+    public Transform player;  // 플레이어의 Transform
+    public float detectionRange = 10.0f;  // 몬스터가 플레이어를 인식하는 범위
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            // 플레이어와의 거리 계산
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (distanceToPlayer <= detectionRange)
+            {
+                // 추적 범위 내에 플레이어가 있는 경우 플레이어를 쫓아감
+                navMeshAgent.SetDestination(player.position);
+            }
+            else
+            {
+                // 추적 범위 밖에 플레이어가 있는 경우 멈춤
+                navMeshAgent.ResetPath();
+            }
+        }
+    }
+}
